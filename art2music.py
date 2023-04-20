291c789b5e5612536e6f9f4d5e8d77443d996f54
itzamorales <130703930+itzamorales@users.noreply.github.com>
2023-04-20 16:12:53 -0400

    Update art2music.py
    Task Layout

--- art2music.py
+++ art2music.py
@@ -4,3 +4,20 @@
 # Date: 4-20-2023
 # Version: 0.1a
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+#Import our libraries
+
+#Keep track of settings
+#Handle user user input
+
+#See if they want to use own image or sample
+#See if we have an image
+
+#Save Image into a temp file
+#Ask the computer to view the image
+#grab Hue from HSV
+#seperate the Hues and convert to Frequency
+#generic song save
+#Make an effected version
+#save the final song and make a midi
