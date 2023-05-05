99699677ed9417df1d5e9d81f5c4a5e370a83bff
itzamorales <130703930+itzamorales@users.noreply.github.com>
2023-05-05 13:48:56 -0400

    Update homepage.py

--- Pages/homepage.py
+++ Pages/homepage.py
@@ -4,4 +4,4 @@ st.set_page_config(
   page_title="Homepage",
   )
 st.title("Welcome to my final project")
-st.sidebar.success("Click on page you'd like to visit."
+st.sidebar.success("Click on page you'd like to visit.")
