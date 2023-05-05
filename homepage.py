e859b8d116113416b42b132e6ecb755d557dee5a
itzamorales <130703930+itzamorales@users.noreply.github.com>
2023-05-05 08:29:50 -0400

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.set_page_config(
+  page_title="Homepage",
+  page_icon=":"beach:", 
+)
+st.title("Welcome to my final project")
+st.sidebar.success("Click on page you'd like to visit."
