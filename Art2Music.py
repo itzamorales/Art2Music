784dc889f0c320a4b161c894a80ee4636d96c28f
itzamorales <130703930+itzamorales@users.noreply.github.com>
2023-05-09 15:54:52 -0400

    123

--- Art2Music.py
+++ Art2Music.py
@@ -25,7 +25,7 @@ import glob
 #Changed note duration & pixels to number pickers
 #Changed a check boxes to Radio option (tried to do both, but they had the same key, so I wasn't able to get them both). 
 #Changed a check box into a dropdown option instead.
-#added cover page to app
+
 
 def get_piano_notes():   
     # White keys are in Uppercase and black keys (sharps) are in lowercase
